2496edfa5af92387702625be3a94507f8a56f973
willswats <williamstuwatson@gmail.com>
2023-02-27 17:47:33 +0000

    Fix shotgun

--- Assets/Scripts/Game/Weapons/WeaponShotgun.cs
+++ Assets/Scripts/Game/Weapons/WeaponShotgun.cs
@@ -4,10 +4,12 @@ using System.Collections.Generic;
 public class WeaponShotgun : Weapon
 {
     public int projectileCount = 3;
-    private List<Projectile> projectiles = new List<Projectile>();
+    private List<Projectile> projectiles;
 
     public override void HandleFire(Vector2 playerPosition, Vector2 playerDirection, Quaternion playerRotation)
     {
+        projectiles = new List<Projectile>();
+
         for (int i = 0; i < projectileCount; i++)
         {
             Vector2 position = playerPosition + playerDirection + new Vector2(i, 0);
@@ -16,6 +18,7 @@ public class WeaponShotgun : Weapon
 
         foreach (Projectile projectile in projectiles)
         {
+            Debug.Log(projectiles);
             projectile.SetForce(playerDirection * this.projectileVelocity);
         }
     }
