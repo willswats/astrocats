b718c3d4f64e40fa9af1d8eb183d7d003bc860b2
willswats <williamstuwatson@gmail.com>
2023-03-07 12:58:08 +0000

    Add code for exploding asteroid

--- Assets/Scripts/Game/Asteroid.cs
+++ Assets/Scripts/Game/Asteroid.cs
@@ -9,11 +9,13 @@ public class Asteroid : MonoBehaviour
     public float lifeTimeSeconds = 30f;
     private SpriteRenderer spriteRenderer;
     private Rigidbody2D rb2d;
+    private Animator anim;
 
     private void Awake()
     {
         this.rb2d = GetComponent<Rigidbody2D>();
         this.spriteRenderer = GetComponent<SpriteRenderer>();
+        this.anim = GetComponent<Animator>();
     }
 
     private void Start()
@@ -27,8 +29,11 @@ public class Asteroid : MonoBehaviour
         string collisionTag = collision.gameObject.tag;
         if (collisionTag == "Projectile")
         {
+            rb2d.simulated = false;
+            GetComponent<Collider2D>().enabled = false;
+            anim.SetTrigger("Explode");
             pointSpawner.Spawn(this.transform.position);
-            Destroy(this.gameObject);
+            Destroy(this.gameObject, 0.5f);
             GameManager.Instance.AddPlayerScore(asteroidScore);
         }
         if (collisionTag == "Player")
