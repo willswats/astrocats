cd2deb6d4ae2a366013ee6a17a98191e6f6d55e4
willswats <williamstuwatson@gmail.com>
2023-03-08 12:01:12 +0000

    Add pauseMenu sounds

--- Assets/Scripts/Game/Pickups/Pickup.cs
+++ Assets/Scripts/Game/Pickups/Pickup.cs
@@ -7,17 +7,20 @@ public class Pickup : MonoBehaviour, IPickup
     public int pickupLifeTimeSeconds = 1;
     private SpriteRenderer spriteRenderer;
     private BoxCollider2D boxCollider2D;
+    private AudioSource audiosource;
 
     private void Start()
     {
         this.spriteRenderer = this.gameObject.GetComponent<SpriteRenderer>();
         this.boxCollider2D = this.gameObject.GetComponent<BoxCollider2D>();
+        this.audiosource = this.gameObject.GetComponent<AudioSource>();
     }
 
     public virtual void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
+            audiosource.Play();
             GameManager.Instance.AddPlayerScore(this.pickupScore);
             PickupDestroy();
         }
--- Assets/Scripts/Game/Pickups/WeaponPickup.cs
+++ Assets/Scripts/Game/Pickups/WeaponPickup.cs
@@ -2,14 +2,11 @@ using UnityEngine;
 
 public class WeaponPickup : Pickup
 {
-    public Weapon weapon;
-
     public override void OnTriggerEnter2D(Collider2D collision)
     {
         base.OnTriggerEnter2D(collision);
         if (collision.gameObject.tag == "Player")
         {
-            Player player = collision.gameObject.GetComponent<Player>();
         }
     }
 }
