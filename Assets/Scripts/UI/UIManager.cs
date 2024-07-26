814219056cf2d32dfe6c9922071aa2483c3ed72f
willswats <william@williamwatson.dev>
2024-07-26 18:48:27 +0100

    refactor: change xp and lives to be more clear

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -19,7 +19,7 @@ public class UIManager : MonoBehaviour
     private string GetPlayerLevelAndExperienceNeeded(int experiencePoints)
     {
         int level = 0;
-        int experiencePointsNeededToLevelUp = 100 - experiencePoints;
+        int experiencePointsNeededToLevelUp = 100;
         if (experiencePoints >= 10000)
         {
             level = 5;
@@ -28,25 +28,25 @@ public class UIManager : MonoBehaviour
         else if (experiencePoints >= 5000)
         {
             level = 4;
-            experiencePointsNeededToLevelUp = 10000 - experiencePoints;
+            experiencePointsNeededToLevelUp = 10000;
         }
         else if (experiencePoints >= 1000)
         {
             level = 3;
-            experiencePointsNeededToLevelUp = 5000 - experiencePoints;
+            experiencePointsNeededToLevelUp = 5000;
         }
         else if (experiencePoints >= 500)
         {
             level = 2;
-            experiencePointsNeededToLevelUp = 1000 - experiencePoints;
+            experiencePointsNeededToLevelUp = 1000;
         }
         else if (experiencePoints >= 100)
         {
             level = 1;
-            experiencePointsNeededToLevelUp = 500 - experiencePoints;
+            experiencePointsNeededToLevelUp = 500;
         }
 
-        return $"{level} ({experiencePointsNeededToLevelUp})";
+        return $"{level} (XP: {experiencePoints}/{experiencePointsNeededToLevelUp})";
     }
 
     public void SetTextTime(float time)
@@ -60,7 +60,7 @@ public class UIManager : MonoBehaviour
 
     public void SetTextPlayerHealth(int health, int lives)
     {
-        this.textPlayerHealth.text = $"Health: {health.ToString()} ({lives.ToString()})";
+        this.textPlayerHealth.text = $"Health: {health.ToString()} (Lives: {lives.ToString()})";
     }
 
     public void SetTextPlayerExperiencePoints(int experiencePoints)
